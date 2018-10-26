f11f378a63faecb6dd8f6a6b14fa0959a2abb215
tar <tar>
2018-10-26 17:54:18 +0800

    extract business logic from manage.py to a new file

---
+++ info/__init__.py
@@ -0,0 +1,21 @@
+# 16. extract business logic from manage.py to a new file
+from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+
+from config import Config
+
+app = Flask(__name__)
+
+# 2. the app loads config the class object
+app.config.from_object(Config)
+# 3. add mySQL
+db = SQLAlchemy(app)
+# 5. initialize redis object
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 7. start CSRF protection for server validation
+CSRFProtect(app)
+# 8. set session to be stored in redis
+Session(app)
--- manage.py
+++ manage.py
@@ -1,26 +1,10 @@
-# extract config class from manage.py to a new file
-from flask import Flask, session
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
+# 15. extract config class from manage.py to a new file
+from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
-
-app = Flask(__name__)
-
-# 2. the app loads config the class object
-app.config.from_object(Config)
-# 3. add mySQL
-db = SQLAlchemy(app)
-# 5. initialize redis object
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 7. start CSRF protection for server validation
-CSRFProtect(app)
-# 8. set session to be stored in redis
-Session(app)
 # 11. create manager to enable command line control
+from info import app, db
+
 manager = Manager(app)
 # 13. use migrate to relate the app and mySQL
 Migrate(app, db)
