43fd05d7ca2dbc5a75eb4b7138ee967fe86c35d8
tar <tar>
2018-10-26 17:03:56 +0800

    configure mySQL

--- manage.py
+++ manage.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    # 1.create a config class
+    DEBUG = True
+    # 4. configure mySQL
+    SQLALCHEMY_DATABASE_URI = "mysql://tar:tar@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+
+# 2. the app loads config the class object
+app.config.from_object(Config)
+# 3. add mySQL
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -9,7 +24,7 @@ def index():
 
 
 def main():
-    app.run(debug=True)
+    app.run()
 
 
 if __name__ == "__main__":
