d8ddf1a0749b0742a11d3c29c7bc47c7b3f8291c
tar <tar>
2018-10-27 09:28:42 +0800

    add favicon.ico

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,11 @@
+from flask import render_template, current_app
+
 from . import index_blue
 
 @index_blue.route('/')
 def index():
-    return "success!"
+    return render_template("news/index.html")
+
+@index_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
