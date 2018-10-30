87db4c6d29799947795b5e92e5ac16fff274d942
tar <tar>
2018-10-30 20:13:53 +0800

    templates fully extracted

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,12 +1,21 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 from info import constants
-from info.models import News
+from info.models import News, User
 from info.modules.news import news_blue
 
 
 @news_blue.route("/<int:news_id>")
 def news_detail(news_id):
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
     # 右侧的新闻排行的逻辑
     news_list = []
     try:
@@ -21,6 +30,7 @@ def news_detail(news_id):
         news_dict_li.append(news.to_basic_dict())
 
     data = {
+        "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
     }
 
