d418dca1638846bdff4d691a6555d29d69522d14
tar <tar>
2018-10-31 11:47:12 +0800

    add collect

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,9 +1,127 @@
-from flask import render_template, current_app, session, g, abort
+from flask import render_template, current_app, session, g, abort, jsonify, request
 
-from info import constants
-from info.models import News, User
+from info import constants, db
+from info.models import News, User, Comment
 from info.modules.news import news_blue
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@news_blue.route('/news_comment', methods=["POST"])
+@user_login_data
+def comment_news():
+    """
+    评论新闻或者回复某条新闻下指定的评论
+    :return:
+    """
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 取到请求参数
+    news_id = request.json.get("news_id")
+    comment_content = request.json.get("comment")
+    parent_id = request.json.get("parent_id")
+
+    # 2. 判断参数
+    if not all([news_id, comment_content]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 查询新闻，并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    # 3. 初始化一个评论模型，并且赋值
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_content
+    if parent_id:
+        comment.parent_id = parent_id
+
+    # 添加到数据库
+    # 为什么要自己去commit()?，因为在return的时候需要用到 comment 的 id
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=comment.to_dict())
+
+
+
+@news_blue.route('/news_collect', methods=["POST"])
+@user_login_data
+def collect_news():
+    """
+    收藏新闻
+    1. 接受参数
+    2. 判断参数
+    3. 查询新闻，并判断新闻是否存在
+    :return:
+    """
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 接受参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    # 2. 判断参数
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 查询新闻，并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    # 4. 收藏以及取消收藏
+    if action == "cancel_collect":
+        # 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        # 收藏
+        if news not in user.collection_news:
+            # 添加到用户的新闻收藏列表
+            user.collection_news.append(news)
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
+
 
 
 @news_blue.route("/<int:news_id>")
@@ -39,10 +157,20 @@ def news_detail(news_id):
     # 更新新闻的点击次数
     news.clicks += 1
 
+    # 判断是否收藏该新闻，默认值为 false
+    is_collected = False
+
+    if user:
+        # 判断用户是否收藏当前新闻，如果收藏：
+        # collection_news 后面可以不用加all，因为sqlalchemy会在使用的时候去自动加载
+        if news in user.collection_news:
+            is_collected = True
+
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
-        "news": news.to_dict()
+        "news": news.to_dict(),
+        "is_collected": is_collected,
     }
 
     return render_template("news/detail.html", data=data)
