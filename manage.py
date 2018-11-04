a0f94e1c5e1929b08a49d1ab32ca52c5567f25c6
tar <tar>
2018-11-04 14:19:21 +0800

    add create manager

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db, models
 
 # 19. create specified name by specified name
+from info.models import User
+
 app = create_app("development")
 
 manager = Manager(app)
@@ -13,8 +15,31 @@ Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.option('-n', '-username', dest="username")
+@manager.option('-p', '-password', dest="password")
+def createsuperuser(username, password):
+    if not all([username, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = username
+    user.mobile = username
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return
+
+    print("添加成功")
+
+
 def main():
-    #12. use manager to run the app
+    # 12. use manager to run the app
     manager.run()
 
 
