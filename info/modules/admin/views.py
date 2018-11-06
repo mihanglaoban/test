5d35ce816a3ff0579afa95dd9fb1bc0ceda309a0
tar <tar>
2018-11-06 19:49:01 +0800

    add news classification

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -3,12 +3,67 @@ from datetime import datetime, timedelta
 
 from flask import request, session, redirect, url_for, render_template, current_app, g, jsonify, abort
 
-from info import constants
+from info import constants, db
 from info.image_storage import storage
 from info.models import User, News, Category
 from info.modules.admin import admin_blue
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
+
+
+@admin_blue.route('/news_type', methods=["GET", "POST"])
+def news_type():
+    """新闻分类的数据展示"""
+    if request.method == "GET":
+        # 查询分类数据
+        try:
+            categories = Category.query.all()
+        except Exception as e:
+            current_app.logger.error(e)
+            return render_template('admin/news_type.html', errmsg="查询数据错误")
+
+        category_dict_li = []
+        for category in categories:
+            # 取到分类的字典
+            cate_dict = category.to_dict()
+            category_dict_li.append(cate_dict)
+
+        # 移除最新的分类
+        category_dict_li.pop(0)
+
+        data = {
+            "categories": category_dict_li
+        }
+
+        return render_template('admin/news_type.html', data=data)
+
+    # 新增或者添加分类
+    # 1. 取参数
+    cname = request.json.get("name")
+    # 如果传了cid，代表是编辑已存在的分类
+    cid = request.json.get("id")
+
+    if not cname:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if cid:
+        # 有 分类 id 代表查询相关数据
+        try:
+            cid = int(cid)
+            category = Category.query.get(cid)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+        if not category:
+            return jsonify(errno=RET.NODATA, errmsg="未查询到分类数据")
+        category.name = cname
+    else:
+        category = Category()
+        category.name = cname
+        db.session.add(category)
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @admin_blue.route('/news_review_action', methods=["POST"])
